f2c6041e199c70166f419522ea36d8ec94e57ba3
Samuel tetenga <126316270+hendrckks@users.noreply.github.com>
2025-01-25 03:17:41 +0300

    edit: "or" background

--- src/components/RoleSelectioDialog.tsx
+++ src/components/RoleSelectioDialog.tsx
@@ -50,7 +50,7 @@ const RoleSelectionDialog = () => {
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-white text-gray-500">Or</span>
+              <span className="px-2 bg-background text-gray-500">Or</span>
             </div>
           </div>
 
