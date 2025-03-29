264697483be08c0e27dd47dc1e25f0fb070bb6e1
hendrckks <samueltetenga@gmail.com>
2025-03-29 20:14:39 +0300

    update: margin props for the main

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -35,7 +35,7 @@ const MainLayout: React.FC = () => {
       {isAuthenticated && <PresenceHandler />}
       <ScrollToTop />
       {!shouldHideNavbar && <Sidebar />}
-      <div className={`flex-1 flex flex-col font-noto ${!shouldHideNavbar ? 'md:ml-[calc(0.5rem+16rem)]' : ''} ${isHomePage ? 'mt-2' : 'mt-2'} overflow-hidden mx-2`}>
+      <div className={`flex-1 flex flex-col font-noto ${!shouldHideNavbar ? 'md:ml-[calc(0.5rem+16rem)]' : ''} ${isHomePage ? 'md:mt-2' : 'md:mt-2'} overflow-hidden md:mx-2`}>
         <main className={`flex-1 font-noto tracking-tight p-4 md:p-6 md:mb-2 mb-0 -mx-5 md:mx-0 ${isHomePage ? 'shadow-lg' : 'bg-white/60 dark:bg-[#111111]  shadow-lg backdrop-blur-3xl'} bg-white/60 dark:bg-[#121212] rounded-xl border dark:border-white/10 border-black/10 overflow-y-auto h-[calc(100vh-1rem)] pb-20 md:pb-6`}>
           {!shouldHideNavbar && (
             <div className="flex mx-auto max-w-7xl justify-between items-center px-2 md:mb-6 gap-4">
